feeeda0ba19d96827153c15ce5cde11ca712aa7d
Adrian <adrianl2710@wp.pl>
2024-04-06 11:20:54 +0200

    Zadanie 2 wykonane

--- Zad1.java
+++ Zad1.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 public class Zad1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Podaj liczbę przedmiotów: ");
         int LiczbaPrzedmiotow = scanner.nextInt();
 
         double sumaOcen = 0;
+        double [] oceny = new double[LiczbaPrzedmiotow];
         
 
-        for (int i = 1; i <= LiczbaPrzedmiotow; i++) {
-            System.out.print("Podaj ocenę z przedmiotu: " + i + ": ");
-            double ocena = scanner.nextDouble();
+        for (int i = 0; i < LiczbaPrzedmiotow; i++) {
+            System.out.print("Podaj ocenę z przedmiotu: " + (i+1) + ": ");
+            oceny[i] = scanner.nextDouble();
+        }
+
+        for (double ocena : oceny) {
             sumaOcen += ocena;
         }
 
